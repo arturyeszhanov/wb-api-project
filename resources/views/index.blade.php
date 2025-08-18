7967ed670353cd7c00249da91396b5a37eec0e20
Artur Yeszhanov <arturyeszhanov@gmail.com>
2025-08-18 09:20:23 +0500

    Добавил favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Vue Dashboard</title>
+    <title>WB API: Dashboard</title>
     @vite('resources/js/app.js')
+    <link rel="icon" href="{{ asset('favicon-32x32.png') }}" type="image/x-icon">
 </head>
 <body class="bg-gray-100">
     <div id="app"></div>
